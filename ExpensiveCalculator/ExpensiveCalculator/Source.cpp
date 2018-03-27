b056281a0748a93a60df216e4d5f08f84b1f23b2
Kane-Greenwalt <kanegreenwalt3@gmail.com>
2018-03-27 13:04:12 -0500

    Expensive Calc

---
+++ ExpensiveCalculator/ExpensiveCalculator/Source.cpp
@@ -0,0 +1,20 @@
+//exoensive calculator
+//today is 3/27/18
+//Author Kane Greenwalt
+
+
+#include<iostream>
+
+int main()
+{
+	std::cout << "7 + 3 = " << 7.0+ 3.0 << std::endl;
+	std::cout << "7 - 3 = " << 7.0 - 3.0 << std::endl;
+	std::cout << "7 * 3 = " << 7.0 * 3.0 << std::endl;
+	std::cout << "7 / 3 = " << 7.0 / 3.0 << std::endl;
+	std::cout << "7 % 3 = " << 7 % 3 << std::endl;
+
+	std::cout << "7 + 3 * 5 = " << 7 + 3 * 5 << std::endl;
+	std::cout << "(7+3) * 5 = " << (7 + 3) * 5 << std::endl;
+	system("Pause");
+	return 0;
+}
