87f98c224996e1c130b1787587e48937cc15e739
Kane-Greenwalt <kanegreenwalt3@gmail.com>
2018-03-28 12:39:11 -0500

    DelgatosTacos Start

---
+++ DelgatosTacos/DelgatosTacos/DelgatosTacos.cpp
@@ -0,0 +1,47 @@
+//DelgatosTacos
+//03/28/18
+//Author Kane Greenwalt
+#include<iostream>
+#include<string>
+using std::cin;
+int main()
+{
+	int NumberOfItem;
+	double tacos = 1.00;
+	double burritos = 1.00;
+	double tostadas = 1.50;
+	double enchiladas = 2.99;
+	double drink = 2.00;
+	double supremeBurrito = 5.50;
+	double potatollas = 1.99;
+	
+	
+	std::cout << "                     ________________________________" << std::endl;
+	std::cout << "                     |           Delgatos           |" << std::endl;
+	std::cout << "                     |            tacos             |" << std::endl;
+	std::cout << "**********************************************************************************" << std::endl;
+	std::cout << "*                                                                                 *" << std::endl;
+	std::cout << "*                                                                                  *" << std::endl;
+	std::cout << "*                    _______________________________                                 *" << std::endl;
+	std::cout << "*                    |                              |                                 *" << std::endl;
+	std::cout << "*                    |                              |                                 *" << std::endl;
+	std::cout << "*                    |                              |                                  **********************" << std::endl;
+	std::cout << "*                    |______________________________|                                                   ***" << std::endl;
+	std::cout << "*                                                                                                      * =*" << std::endl;
+	std::cout << "*                                                                                                      ****" << std::endl;
+	std::cout << "*                                                                                                         *" << std::endl;
+	std::cout << "*                                                                                                         *" << std::endl;
+	std::cout << "*                                                                                                         *" << std::endl;
+	std::cout << "*   ********                                                                             ********        *" << std::endl;
+	std::cout << "*****      *******************************************************************************      **********" << std::endl;
+	std::cout << "    * 0000 *                                                                             * 0000 *         " << std::endl;
+
+
+
+
+
+
+
+	system("Pause");
+	return 0;
+}
